93954871ce2392f31bf9f9fe4548498b2e2e9185
Olga Kulczycka <olga@webjaksklep.eu>
2025-11-05 23:41:52 +0100

    questions fetching & start component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 import Header from './Header';
 import Main from './Main';
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+
+const initialState = {
+  questions: [],
+  status: 'loading', // loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'dataRecieved':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    case 'dataActive': 
+      return {
+        ...state,
+        status: 'active'
+      }
+    default: 
+      throw new Error(`Unknown action`);
+  }
+}
 
 export default function App() {
+  const [{questions, status}, dispatch] = useReducer(reducer, initialState);
+
   useEffect(() => {
-    fetch('questions.json')
-    .then((res) => res.json())
-    .then((data) => console.log(data))
+    fetch('http://localhost:8000/questions')
+    .then((data) => data.json())
+    .then((data) => dispatch({type: 'dataRecieved', payload: data}))
+    .catch((err) => dispatch({type: 'dataFailed'}))
   }, []);
+
+  const numQuestions = questions.length;
+
   return (
     <div className="app">
       <Header/>
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader/>}
+        {status === 'error' && <Error/>}
+        {status === 'ready' && <StartScreen numQuestions={numQuestions} dispatch={dispatch}/>}
+        {status === 'active' && <Question/>}
       </Main>
     </div>
   )
