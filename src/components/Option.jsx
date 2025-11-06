57902f24972c9edf23a6099efb71f6442b771c76
Olga Kulczycka <olga@webjaksklep.eu>
2025-11-06 14:54:15 +0100

    questions answers & next question

---
+++ src/components/Option.jsx
@@ -0,0 +1,12 @@
+function Options({question, dispatch, answer}) {
+    const hasAnswered = answer !== null;
+    return (
+        <div className="options">
+            {question.options.map((option, index) => (
+                <button className={`btn btn-option ${index === answer ? 'answer' : ''} ${hasAnswered ? index === question.correctOption ? 'correct' : 'wrong' : ''}`} key={index} onClick={() => dispatch({type: 'newAnswer', payload: index})}>{option}</button>
+            ))}
+        </div>
+    )
+}
+
+export default Options;
